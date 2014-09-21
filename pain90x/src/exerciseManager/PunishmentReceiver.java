5d9ee220b89c03a729d548dfa792fba6ac615e05
lingzhong <ling.j.zhong@gmail.com>
2014-09-21 00:06:32 -0700

    tailored FB post functionality for demo

--- pain90x/src/exerciseManager/PunishmentReceiver.java
+++ pain90x/src/exerciseManager/PunishmentReceiver.java
@@ -3,6 +3,7 @@ package exerciseManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.widget.Toast;
 
@@ -18,7 +19,7 @@ public class PunishmentReceiver extends BroadcastReceiver {
         }
     }
 
-    private void punish(Context context, Intent intent) {
+    private void punishTest(Context context, Intent intent) {
         String toastMsg = "YOU ARE BEING PUNISHED BECAUSE YOU HAVEN'T COMPLETED THE "
                 + intent.getStringExtra(ToDoItem.TITLE) + " TASK!!";
         Toast.makeText(context, toastMsg, Toast.LENGTH_LONG).show();
@@ -40,6 +41,25 @@ public class PunishmentReceiver extends BroadcastReceiver {
             Toast.makeText(context, "SMS failed, please try again.",
                     Toast.LENGTH_LONG).show();
             e.printStackTrace();
+        }
+    }
+
+    private void punishFB(Context context, Intent intent) {
+        // toast
+        String toastMsg = "YOU ARE BEING PUNISHED ON FB BECAUSE YOU HAVEN'T COMPLETED THE "
+                + intent.getStringExtra(ToDoItem.TITLE) + " TASK!!";
+        Toast.makeText(context, toastMsg, Toast.LENGTH_LONG).show();
+        // post on FB
+        facebookClient = new Facebook();
+        facebookClient.authorize(this, new String[] { "publish_stream",
+                "read_stream", "offline_access" }, this);
+        String message = "I have big butts I cannot lie!";
+        try {
+            Bundle parameters = new Bundle();
+            parameters.putString("message", message);
+            facebookClient.dialog(this, "stream.publish", parameters, this);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
